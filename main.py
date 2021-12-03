3d3fea6313a4c942129ddddfbb3e1463d1ba27e1
Shashank Sharma <78837537+theshashankk@users.noreply.github.com>
2021-12-04 00:12:40 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+import os
+
+os.system('git clone https://github.com/theshashankk/StringSessionBot.git && cd StringSessionBot')
+print('[INFO] ------ GIT CLONING')
+os.system('python3 generator.py')
+print('[LOGS] ------ PROCESSING')
