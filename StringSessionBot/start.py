e7d0ecbc6e0f69aaa98b749d03e128639a4b7e34
Shashank Sharma <78837537+theshashankk@users.noreply.github.com>
2021-12-03 23:46:16 +0530

    Update start.py

--- StringSessionBot/start.py
+++ StringSessionBot/start.py
@@ -1,15 +1,14 @@
 from Data import Data
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+k_but = InlineKeyboardMarkup([[InlineKeyboardButton('Generate string', callback_data='generate')]])
+	
+	
 
 # Start Message
 @Client.on_message(filters.private & filters.incoming & filters.command("start"))
 async def start(bot, msg):
 	user = await bot.get_me()
 	mention = user["mention"]
-	await bot.send_message(
-		msg.chat.id,
-		Data.START.format(msg.from_user.mention, mention),
-		reply_markup=InlineKeyboardMarkup(Data.buttons)
-	)
+	await bot.reply_text(f'Hey {msg.from_user.mention}\n\n__Click on Below Button to use me__', reply_markup=k_but)
